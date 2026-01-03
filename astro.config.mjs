cf198107abc73d18285c044cc527644f6dbf0088
bryan <bryanvivanco97@outlook.com>
2026-01-03 16:11:50 -0500

    robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,26 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+   output: "server",
 
-  vite: {
-   plugins: [tailwindcss()],
-  },
+   vite: {
+      plugins: [tailwindcss()],
+   },
 
-  integrations: [react()],
+   integrations: [
+      react(),
+      sitemap(),
+],
 
-  env: {
-     schema: {
-        WEB3FORMS_KEY: envField.string({ context: 'server', access: 'public' }),
-     }
-  },
+   env: {
+      schema: {
+         WEB3FORMS_KEY: envField.string({ context: 'server', access: 'public' }),
+      }
+   },
 
-  adapter: vercel(),
+   adapter: vercel(),
 });
